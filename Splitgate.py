dd6415d09be4caf4c557e0d9654c0f8c304c8963
Bboddy <43559704+Bboddy@users.noreply.github.com>
2021-08-26 21:45:19 -0700

    Changed Imagecapture Lib to MSS
    Mss is 100x faster and better at grabbing  a image from the screen, still using pillow for image processing / evaluation. With mss i am now only grabing a 4x4 pixel ss of the screen instead of the whole thing.

--- Splitgate.py
+++ Splitgate.py
@@ -1,53 +1,46 @@
-from PIL import ImageGrab
+from PIL import Image
 from ctypes import windll
-import time, pyttsx3, win32api
+import time, pyttsx3, win32api, mss, mss.tools
 
-engine = pyttsx3.init()
-engine.setProperty("volume", 0.5)
-engine.setProperty("rate", 350)
-voices = engine.getProperty("voices")
-engine.setProperty("voice", voices[1].id)
-#Voice welcome
-engine.say("Started")
-engine.runAndWait()
-
-def GetPixel(x,y):
-    image = ImageGrab.grab()
-    color = image.getpixel((x, y))
-    return color
-
-red = (254, 0, 0)
-red2 = (255, 0, 0)
-
-stopShooting = False
+isShooting = False
 paused = True
 active = True
+red = (255, 0, 0)
 
 def run():
-    global paused, stopShooting, active
+    global paused, isShooting, active
+    # Voice Settings
+    engine = pyttsx3.init()
+    engine.setProperty("volume", 0.5)
+    engine.setProperty("rate", 350)
+    voices = engine.getProperty("voices")
+    engine.setProperty("voice", voices[1].id)
+    # Voice Welcome
+    engine.say("Started")
+    engine.runAndWait()
 
-    #Voice settings
     while active:
         while not paused:
-            time.sleep(0.1)
-            color = GetPixel(1280 , 720)
-            if color == red or color == red2:
-                color = GetPixel(1280 , 720)
-                stopShooting = True
-                #print("Shoot")
+            with mss.mss() as sct:
+                monitor = {"top": 1280, "left": 720, "width": 4, "height": 4}
+                sct_img = sct.grab(monitor)
+                img = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")
+                color = img.getpixel((2, 2))
+            if color == red:
+                isShooting = True
                 windll.user32.mouse_event(0x0002,0,0,0,0)
-            if stopShooting == True:
+            if isShooting == True:
                 windll.user32.mouse_event(0x0004,0,0,0,0)
-                stopShooting = False
-            if win32api.GetKeyState(0x13) < 0: #Pause
+                isShooting = False
+            if win32api.GetKeyState(0x13) < 0 and paused == False:
                 paused = True
                 engine.say("Paused")
                 engine.runAndWait()
-        if win32api.GetKeyState(0x13) < 0: #Pause
+        if win32api.GetKeyState(0x13) < 0 and paused == True:
                 paused = False
                 engine.say("Unpaused")
                 engine.runAndWait()
-        if win32api.GetKeyState(0x23) < 0: #End 
+        if win32api.GetKeyState(0x23) < 0:
             engine.say("Exiting")
             engine.runAndWait()
             active = False
